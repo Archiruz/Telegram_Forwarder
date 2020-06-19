4bf3f2cd78cdaafc01d8877f0d5861e335a23057
Archiruz <47630429+Archiruz@users.noreply.github.com>
2020-06-19 21:20:11 +0700

    initial config

--- forwarder/config.py
+++ forwarder/config.py
@@ -1,35 +1,12 @@
-if not __name__.endswith("sample_config"):
-    import sys
-    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
-          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
-    quit(1)
-
-
-# Create a new config.py file in same dir and import, then extend this class.
-class Config(object):
-    LOGGER = True
-
-    # REQUIRED
-    API_KEY = "617722711:AAHysCHsVcQja6LOlHa88101lKjrxez80-M"  # API key obtained from BotFather
-    OWNER_ID = "582884567"  # If you dont know, run the bot and do /id in your private chat with the bot
-
-    # FOR AUTOMATICALLY FORWARDING MESSAGES
-    FROM_CHATS = [-1001234704297 ]  # List of chat id's to forward messages from
-    TO_CHATS = [-1001128355490]  # List of chat id's to forward messages to
-
-    # FOR WEBHOOKS
-    WEBHOOK = False
-    IP_ADDRESS = "127.0.0.1"  # Use "0.0.0.0" if using Heroku
-    URL = None  # The URL that the bot should listen to for updates
-    PORT = 5000  # Port to listen on for webhooks
-    CERT_PATH = None  # Path to certificate file
-
-    WORKERS = 4  # Depends on usage, 4 by default
-
-
-class Production(Config):
-    LOGGER = False
+from auto_forwarder.sample_config import Config
 
 
 class Development(Config):
-    LOGGER = True
+    API_KEY = "1155370524:AAEFB-Ntg33e8KNbz40cKQZjvzIeTt3rQFo"  # Your bot API key
+    OWNER_ID = 888449576  # Your user id
+
+    # Make sure to include the '-' sign in group and channel ids.
+    FROM_CHATS = [-1001234567890]  # List of chat id's to forward messages from.
+    TO_CHATS = [-1001453770633]  # List of chat id's to forward messages to.
+    
+    WORKERS = 4
